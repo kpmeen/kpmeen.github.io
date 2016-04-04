467f27cbb9707126036d54e0e9ebb56401de7c15
Knut Petter Meen <kp@scalytica.net>
2016-04-04 10:13:36 +0200

    have to make the shadow smaller

--- _project/src/main/scala/net/scalytica/blaargh/styles/BlaarghBootstrapCSS.scala
+++ _project/src/main/scala/net/scalytica/blaargh/styles/BlaarghBootstrapCSS.scala
@@ -30,7 +30,7 @@ object BlaarghBootstrapCSS extends StyleSheet.Inline {
     val labelInfo = mixin(addClassNames("label", "label-info"))
 
     val cardShadow = mixin(
-      boxShadow := "5px 5px 10px 0px rgba(173,173,173,0.6)"
+      boxShadow := "3px 3px 6px 0px rgba(173,173,173,0.6)"
     )
 
     val easeOutAnimation = mixin(
