a58ed5a045d541b7cc184974ae44a0d8332a15cd
Knut Petter Meen <kp@scalytica.net>
2016-04-11 11:05:08 +0200

    Now generating sitemap.xml

--- _build/paths.scala
+++
@@ -1,8 +0,0 @@
-@
-import ammonite.ops._
-
-val postsFolder = cwd / up / '_posts
-val postsTargetFolder = cwd / up / 'posts
-
-val pagesFolder = cwd / up / '_pages
-val pagesTargetFolder = cwd / up / 'pages
